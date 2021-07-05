7c2032fc8c1e4fcaff098e27f783dd7724bc064b
Brian Turner <brianturner818@gmail.com>
2021-07-06 03:01:55 +0800

    add support for reloading commands and permissions

--- src/main/kotlin/org/sanguineous/jscommand/JsCommand.kt
+++ src/main/kotlin/org/sanguineous/jscommand/JsCommand.kt
@@ -1,35 +1,46 @@
 package org.sanguineous.jscommand
 
-import net.md_5.bungee.api.ProxyServer
 import net.md_5.bungee.api.plugin.Plugin
 import org.sanguineous.jscommand.command.JavascriptCommand
+import org.sanguineous.jscommand.command.ReloadCommand
 import java.io.File
 import java.util.*
 
 class JsCommand : Plugin() {
 
+    private var commands = mutableListOf<JavascriptCommand>()
+
     override fun onEnable() {
         loadCommands()
+        proxy.pluginManager.registerCommand(this, ReloadCommand(this))
     }
 
-    private fun loadCommands() {
+    fun loadCommands() {
         if (!dataFolder.exists())
             dataFolder.mkdir()
         val dir = File(dataFolder, "scripts")
         if (!dir.exists())
             dir.mkdir()
         val files = dir.listFiles()
+        for (command in commands) {
+            proxy.pluginManager.unregisterCommand(command)
+        }
+        commands.clear()
         for (file in files) {
             if (!file.isFile)
                 return
-            val fileName = file.name.substring(0, file.name.indexOf("."))
+            val fileName = file.name.substring(0, file.name.indexOf(".")).toLowerCase()
+            if (proxy.pluginManager.isExecutableCommand(fileName, proxy.console)) {
+                println("Found command ${fileName}, registering anyways")
+            }
             val contents = StringBuilder()
             val scanner = Scanner(file)
             while (scanner.hasNextLine()) {
                 contents.append("${scanner.nextLine()}\n")
             }
-            ProxyServer.getInstance().pluginManager
-                    .registerCommand(this, JavascriptCommand(this, fileName, contents.toString()))
+            val command = JavascriptCommand(this, fileName, contents.toString())
+            proxy.pluginManager.registerCommand(this, command)
+            commands.add(command)
         }
     }
 }
--- src/main/kotlin/org/sanguineous/jscommand/command/JavascriptCommand.kt
+++ src/main/kotlin/org/sanguineous/jscommand/command/JavascriptCommand.kt
@@ -1,12 +1,14 @@
 package org.sanguineous.jscommand.command
 
 import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.plugin.Command
 import org.mozilla.javascript.engine.RhinoScriptEngineFactory
 import org.sanguineous.jscommand.JsCommand
 import javax.script.ScriptEngineManager
 
-class JavascriptCommand(private val plugin: JsCommand, fileName: String, private val contents: String) : Command(fileName) {
+class JavascriptCommand(private val plugin: JsCommand, fileName: String, private val contents: String)
+    : Command(fileName, "jscommand.command.${fileName}") {
     override fun execute(sender: CommandSender, args: Array<String>) {
         val manager = ScriptEngineManager()
         manager.registerEngineName("rhino", RhinoScriptEngineFactory())
@@ -14,7 +16,8 @@ class JavascriptCommand(private val plugin: JsCommand, fileName: String, private
         engine.put("sender", sender)
         engine.put("plugin", plugin)
         engine.put("args", args)
-        engine.put("server", plugin.proxy)
+        engine.put("isConsole", sender !is ProxiedPlayer)
+        engine.put("proxy", plugin.proxy)
         engine.eval(contents)
     }
 }
