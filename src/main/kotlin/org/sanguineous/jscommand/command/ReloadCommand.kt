5d8155557ebc44b620f99fe42ac3705185d3a098
Brian Turner <brianturner818@gmail.com>
2021-07-07 22:07:38 +0800

    change reload command

--- src/main/kotlin/org/sanguineous/jscommand/command/ReloadCommand.kt
+++ src/main/kotlin/org/sanguineous/jscommand/command/ReloadCommand.kt
@@ -5,11 +5,11 @@ import net.md_5.bungee.api.chat.ComponentBuilder
 import net.md_5.bungee.api.plugin.Command
 import org.sanguineous.jscommand.JsCommand
 
-class ReloadCommand(private val plugin: JsCommand) : Command("jscommand") {
+class ReloadCommand(private val plugin: JsCommand) : Command("jscommand", "jscommand.reload") {
     override fun execute(sender: CommandSender?, args: Array<String>?) {
-        if (args?.get(0) == "reload" && sender!!.hasPermission("jscommand.reload")) {
-            sender!!.sendMessage(*ComponentBuilder("Successfully reloaded commands").create())
+        if (args?.get(0) == "reload") {
             plugin.loadCommands()
+            sender!!.sendMessage(*ComponentBuilder("Successfully reloaded commands").create())
         }
     }
 }
